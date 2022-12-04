c441245b7a530845337441f3dc581d827d0ffb28
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-12-04 08:17:50 +0100

    SDF

--- index.js
+++ index.js
@@ -1,42 +1,41 @@
 import express from "express";
-// const app = express();
+const app = express();
 const PORT = process.env.PORT || 5000;
 import dotenv from "dotenv";
-// import dbConnect from "./db/dbConnect.js";
-// import http from "http";
-// import { Server, Socket } from "socket.io";
-// import Router from "./socket/routers/Router.js";
-// import socketFuncs from "./socket/socket-funcs/socketFuncs.js";
-// import userModal from "./db/schema/userModal.js";
+import dbConnect from "./db/dbConnect.js";
+import http from "http";
+import { Server, Socket } from "socket.io";
+import Router from "./socket/routers/Router.js";
+import socketFuncs from "./socket/socket-funcs/socketFuncs.js";
+import userModal from "./db/schema/userModal.js";
 
 dotenv.config();
 const ORIGIN = process.env.ORIGIN;
 
-// app.use(express.json());
+app.use(express.json());
 
-// dbConnect();
+dbConnect();
 
-// const server = express();
-//http.createServer(app);
+const server = http.createServer(app);
 
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
-// const io = new Server(server, {
-//   cors: {
-//     origins: [`${ORIGIN}`],
-//   },
-// });
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+const io = new Server(server, {
+  cors: {
+    origins: [`${ORIGIN}`],
+  },
+});
 
-// io.on("connection", (socket) => {
-//   socketFuncs(io, socket);
-//   console.log("new user connected");
-// });
+io.on("connection", (socket) => {
+  socketFuncs(io, socket);
+  console.log("new user connected");
+});
 
 // app.use(
 //   bodyParser.json({
@@ -45,17 +44,17 @@ const ORIGIN = process.env.ORIGIN;
 // );
 //:
 
-// app.use("/", Router);
-// app.get("/api/user/senduser", (req, res) => {
-//   console.log("users");
-//   userModal.find({}).then((users) => {
-//     console.log("users", users);
-//     res.json({ data: users });
-//   });
-// });
+app.use("/", Router);
+app.get("/api/user/senduser", (req, res) => {
+  console.log("users");
+  userModal.find({}).then((users) => {
+    console.log("users", users);
+    res.json({ data: users });
+  });
+});
 
-const server = express();
-server.get("/", (req, res) => {
+//
+app.get("/", (req, res) => {
   console.log("users");
   res.json({ data: "DF" });
 });
