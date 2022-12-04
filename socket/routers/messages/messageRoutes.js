abe4ebe8f2156a7b0295713dba4ce2e07ed8d2f0
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-12-04 08:42:06 +0100

    DF

--- socket/routers/messages/messageRoutes.js
+++ socket/routers/messages/messageRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
 import messageGets from "./get/messageGets.js";
-// import messagePosts from "./post/messagePosts.js";
+import messagePosts from "./post/messagePosts.js";
 const messageRoutes = express.Router();
 
 messageRoutes.use("/", messageGets);
-// messageRoutes.use("/", messagePosts);
+messageRoutes.use("/", messagePosts);
 
 messageRoutes.get("/api/msg", (req, res) => {
   res.json("sdpo");
