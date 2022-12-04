280cb1df0fce518b9afeff7423e1012096f23f7d
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-12-04 08:40:47 +0100

    SD

--- socket/routers/messages/messageRoutes.js
+++ socket/routers/messages/messageRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
-// import messageGets from "./get/messageGets.js";
+import messageGets from "./get/messageGets.js";
 // import messagePosts from "./post/messagePosts.js";
 const messageRoutes = express.Router();
 
-// messageRoutes.use("/", messageGets);
+messageRoutes.use("/", messageGets);
 // messageRoutes.use("/", messagePosts);
 
 messageRoutes.get("/api/msg", (req, res) => {
