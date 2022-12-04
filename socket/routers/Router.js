6ff4d98a0611933f320601e78f954dc8b25b7f2e
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-12-04 08:30:15 +0100

    DF

--- socket/routers/Router.js
+++ socket/routers/Router.js
@@ -1,10 +1,10 @@
 import express from "express";
-// import authRoutes from "./auth/authRoutes.js";
+import authRoutes from "./auth/authRoutes.js";
 // import messageRoutes from "./messages/messageRoutes.js";
 const Router = express.Router();
 
 // Router.use("/api", messageRoutes);
-// Router.use("/api", authRoutes);
+Router.use("/api", authRoutes);
 Router.get("/api", (req, res) => {
   res.json("sdpo");
 });
