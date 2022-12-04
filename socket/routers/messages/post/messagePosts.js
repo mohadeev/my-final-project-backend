f4efed6707665ab6cbf43d901f1ca6c7ebb4ac0a
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-12-04 08:48:58 +0100

    DF

--- socket/routers/messages/post/messagePosts.js
+++ socket/routers/messages/post/messagePosts.js
@@ -1,36 +1,39 @@
 import express from "express";
 import verifyUser from "../../../../utils/verify-user/verifyUser.js";
 import createConversation from "./createConversation.js";
-import createMessage from "./createmessage.js";
+import createMessage from "./createMessage.js";
 const messagePosts = express.Router();
 
-const allRoutes = [
-  {
-    name: createConversation,
-    auth: true,
-    rout: "/create-conversation",
-  },
-  {
-    name: createMessage,
-    auth: true,
-    rout: "/create-message",
-  },
-];
+messagePosts.use("/", verifyUser, createConversation);
+// messagePosts.use("/", verifyUser, createMessage);
 
-allRoutes.map(({ name, auth, rout }) => {
-  if (auth) {
-    if (rout.length >= 2) {
-      messagePosts.use(`/post/message${rout}/:token`, verifyUser, name);
-    } else {
-      messagePosts.use("/", verifyUser, name);
-    }
-  } else {
-    if (rout.length >= 2) {
-      messagePosts.use(`/post/message/${rout}/`, name);
-    } else {
-      messagePosts.use("/", name);
-    }
-  }
-});
+// const allRoutes = [
+//   {
+//     name: createConversation,
+//     auth: true,
+//     rout: "/create-conversation",
+//   },
+//   {
+//     name: createMessage,
+//     auth: true,
+//     rout: "/create-message",
+//   },
+// ];
+
+// allRoutes.map(({ name, auth, rout }) => {
+//   if (auth) {
+//     if (rout.length >= 2) {
+//       messagePosts.use(`/post/message${rout}/:token`, verifyUser, name);
+//     } else {
+//       messagePosts.use("/", verifyUser, name);
+//     }
+//   } else {
+//     if (rout.length >= 2) {
+//       messagePosts.use(`/post/message/${rout}/`, name);
+//     } else {
+//       messagePosts.use("/", name);
+//     }
+//   }
+// });
 
 export default messagePosts;
