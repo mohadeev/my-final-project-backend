9a1cd781e9679c33f61e19a400060d1127eca65b
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-12-04 11:10:23 +0100

    DF

--- socket/routers/messages/post/createConversation.js
+++ socket/routers/messages/post/createConversation.js
@@ -5,55 +5,55 @@ import messageModal from "../../../../db/schema/messageModal.js";
 import userModal from "../../../../db/schema/userModal.js";
 const createConversation = express.Router();
 
-// createConversation.post("/", async (req, res) => {
-//   const { receiver } = req.body;
-//   const reqUserId = req.userId;
-//   console.log("receiver", receiver);
-//   console.log("reqUserId", reqUserId);
-//   await userModal.findOne({ _id: reqUserId }).then(async (useDoc) => {
-//     const findReceiverData = await userModal
-//       .findOne({ email: receiver })
-//       .then((recieverData) => {
-//         if (recieverData) {
-//           return recieverData;
-//         } else {
-//           return null;
-//         }
-//       });
-//     if (useDoc && findReceiverData) {
-//       converstionsModal
-//         .findOne({
-//           members: { $all: [findReceiverData?._id.toString("hex"), reqUserId] },
-//         })
-//         .then(async (conversation) => {
-//           if (conversation) {
-//             console.log("conversation", conversation);
-//             res.json({ responseData: conversation });
-//           } else if (!conversation) {
-//             console.log("sorry no conversation", conversation);
-//             try {
-//               await converstionsModal
-//                 .create({
-//                   members: [reqUserId, findReceiverData?._id.toString("hex")],
-//                 })
-//                 .then((newConversation) => {
-//                   console.log("new conv", newConversation);
-//                   res.json({
-//                     responseData: {
-//                       newConversation: true,
-//                       conversationData: { newConversation },
-//                       userData: findReceiverData,
-//                     },
-//                   });
-//                 });
-//             } catch (error) {}
-//           }
-//         });
-//     } else {
-//       res.json({ errormessage: "NO-USER-FOUNDED" });
-//     }
-//   });
-// });
+createConversation.post("/", async (req, res) => {
+  // const { receiver } = req.body;
+  // const reqUserId = req.userId;
+  // console.log("receiver", receiver);
+  // console.log("reqUserId", reqUserId);
+  // await userModal.findOne({ _id: reqUserId }).then(async (useDoc) => {
+  //   const findReceiverData = await userModal
+  //     .findOne({ email: receiver })
+  //     .then((recieverData) => {
+  //       if (recieverData) {
+  //         return recieverData;
+  //       } else {
+  //         return null;
+  //       }
+  //     });
+  //   if (useDoc && findReceiverData) {
+  //     converstionsModal
+  //       .findOne({
+  //         members: { $all: [findReceiverData?._id.toString("hex"), reqUserId] },
+  //       })
+  //       .then(async (conversation) => {
+  //         if (conversation) {
+  //           console.log("conversation", conversation);
+  //           res.json({ responseData: conversation });
+  //         } else if (!conversation) {
+  //           console.log("sorry no conversation", conversation);
+  //           try {
+  //             await converstionsModal
+  //               .create({
+  //                 members: [reqUserId, findReceiverData?._id.toString("hex")],
+  //               })
+  //               .then((newConversation) => {
+  //                 console.log("new conv", newConversation);
+  //                 res.json({
+  //                   responseData: {
+  //                     newConversation: true,
+  //                     conversationData: { newConversation },
+  //                     userData: findReceiverData,
+  //                   },
+  //                 });
+  //               });
+  //           } catch (error) {}
+  //         }
+  //       });
+  //   } else {
+  //     res.json({ errormessage: "NO-USER-FOUNDED" });
+  //   }
+  // });
+});
 
 createConversation.get("/conversion", function (req, res) {
   res.json("here");
