9b607958fa3fe8406dcfbf17e5f8dc6888ef69b4
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-12-04 07:40:19 +0100

    DF

--- index.js
+++ index.js
@@ -16,26 +16,27 @@ app.use(express.json());
 
 dbConnect();
 
-const server = http.createServer(app);
+const server = express();
+//http.createServer(app);
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-const io = new Server(server, {
-  cors: {
-    origins: [`${ORIGIN}`],
-  },
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+// const io = new Server(server, {
+//   cors: {
+//     origins: [`${ORIGIN}`],
+//   },
+// });
 
-io.on("connection", (socket) => {
-  socketFuncs(io, socket);
-  console.log("new user connected");
-});
+// io.on("connection", (socket) => {
+//   socketFuncs(io, socket);
+//   console.log("new user connected");
+// });
 
 // app.use(
 //   bodyParser.json({
@@ -44,16 +45,16 @@ io.on("connection", (socket) => {
 // );
 //:
 
-app.use("/", Router);
-app.get("/api/user/senduser", (req, res) => {
-  console.log("users");
-  userModal.find({}).then((users) => {
-    console.log("users", users);
-    res.json({ data: users });
-  });
-});
+// app.use("/", Router);
+// app.get("/api/user/senduser", (req, res) => {
+//   console.log("users");
+//   userModal.find({}).then((users) => {
+//     console.log("users", users);
+//     res.json({ data: users });
+//   });
+// });
 
-app.get("/", (req, res) => {
+server.get("/", (req, res) => {
   console.log("users");
   res.json({ data: "DF" });
 });
